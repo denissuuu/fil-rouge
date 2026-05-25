f2d26c5d4398173a8da97f7baf575a8957983dff
odenis <denis.ont@ynov.com>
2026-05-25 16:17:15 +0200

    modified auto passw

--- backend/config/ApplicationConfig.java
+++ backend/config/ApplicationConfig.java
@@ -27,11 +27,10 @@ public class ApplicationConfig {
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(passwordEncoder());
         authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
-    }
+}
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
